05685ee32f434f743459e2a8e595fda4b507ea4d
Richard Yang <im.richardryang@gmail.com>
2020-07-10 19:21:52 -0700

    loops through list of messages and displays

--- Arduino_OLED_Project.ino
+++ Arduino_OLED_Project.ino
@@ -34,15 +34,27 @@ void setup()  {
 
   oled.Device_Init();
 
-  oled.Set_Color(VICTORIA_BLUE);
-
-  char* str[3] = {"i love", "you more", "than anything"};
-  for(int i = 0; i < 3; i ++){
-    int start = 64 - strlen(str[i]) * 2.5;
-    oled.print_String(start, (i + 3) * 15, str[i], FONT_5X8);
+  char* str[24][3] = {{"you", "are my", "everything"}, {"i", "love", "you!"}, {"i", "would eat", "durian for you"},
+                    {"i love", "planning for", "our future together"}, {"carrying", "you is my", "favorite workout"},
+                    {"whenever", "you're with me", "i feel safe"}, {"whenever", "you need me", "i'll be there"},
+                    {"you look so", "stunning in", "anything you wear"}, {"i think", "you have", "long legs!"},
+                    {"you'll", "always be", "my best friend"}, {"you're so", "cute when", "you're passionate"},
+                    {"being intimate", "with you makes my", "heart flutter A LOT"}, {"you make", "me laugh like", "no one else"},
+                    {"saying goodnight", "is the hardest", "part of my day"}, {"you have", "the prettiest", "voice in the world"},
+                    {"i", "wanna cuddle", "you forever"}, {"you", "are", "a-dory-able!"}, {"i love", "you more than", "you love noodles!"},
+                    {"my worst", "day will never", "come if im with you"}, {"our 3 dogs, 1 (maybe)", "cat, and fish will", "have the bestest mom!"},
+                    {"your", "smile is", "unbelievably pretty"}, {"nature is", "prettier when i'm", "hiking with you"},
+                    {"i blush when", "you tell me to wait", "a sex- oop, SEC ;)"}, {"the only time i want", "to do the dishes is", "when you tell me to"}};
+  for(int i = 0; i < 24; i ++){
+    oled.Set_Color(VICTORIA_BLUE);
+    for(int j = 0; j < 3; j ++){
+      int start = 64 - strlen(str[i][j]) * 2.88;
+      oled.print_String(start, (j + 3.2) * 15, str[i][j], FONT_5X8);
+    }
+    oled.Draw_Heart();
+    delay(1000);
+    oled.Clear_Screen();
   }
-
-  oled.Draw_Heart();
 }
 
 
